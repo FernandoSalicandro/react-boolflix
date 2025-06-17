b00e93c7b08a7f4b6ac946d0046592fb8958e598
FernandoSalicandro <studio.salicandro@gmail.com>
2025-06-17 17:44:22 +0200

    feat : implementato rendering del voto in stelle piene e vuote

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -5,36 +5,48 @@ import axios from 'axios'
 
 
 export default function HomePage() {
-  const { movie } = useContext(FilmContext);
+    const { movie } = useContext(FilmContext);
 
-  return (
-    <>
-      {movie && movie.length > 0 && <h1 className="mb-4">La Tua Ricerca</h1>}
+    const voteRendering = (film) => {
+        const toFive = Math.ceil(parseFloat(film.vote_average) / 2);
+        const emptyStars = 5 - toFive;
 
-        <div className="container-fluid">
-        <div className="row row-cols-6 gap-1">
-          {movie &&
-            movie.length > 0 &&
-            movie.map((curMovie) => (
-              <div key={curMovie.id} className="card movie-card">
-                <img
-                  className="img-fluid"
-                  src={`https://image.tmdb.org/t/p/w342${curMovie.poster_path}`}
-                  alt={curMovie.title}
-                />
-                <div className="movie-details">
-                  <p><strong>Titolo Originale:</strong> {curMovie.original_title}</p>
-                  <p><strong>Titolo:</strong> {curMovie.title}</p>
-                  <p><strong>Lingua:</strong> {curMovie.original_language}</p>
-                  <p><strong>Voto:</strong> {curMovie.vote_average}</p>
+        const voteArr = new Array(toFive).fill(<i class="fa-solid fa-star"></i>)
+        const emptyArr = new Array(emptyStars).fill(<i class="fa-regular fa-star"></i>)
+        const finalStars = [...voteArr, ...emptyArr]
+        return finalStars
+
+    }
+
+
+    return (
+        <>
+            {movie && movie.length > 0 && <h1 className="mb-4">La Tua Ricerca</h1>}
+
+            <div className="container-fluid">
+                <div className="row row-cols-6 gap-1">
+                    {movie &&
+                        movie.length > 0 &&
+                        movie.map((curMovie) => (
+                            <div key={curMovie.id} className="card movie-card">
+                                <img
+                                    className="img-fluid"
+                                    src={`https://image.tmdb.org/t/p/w342${curMovie.poster_path}`}
+                                    alt={curMovie.title}
+                                />
+                                <div className="movie-details">
+                                    <p><strong>Titolo Originale:</strong> {curMovie.original_title}</p>
+                                    <p><strong>Titolo:</strong> {curMovie.title}</p>
+                                    <p><strong>Lingua:</strong> {curMovie.original_language}</p>
+                                    <p><strong>Voto:</strong> {voteRendering(curMovie)}</p>
+                                </div>
+                            </div>
+                        ))}
                 </div>
-              </div>
-            ))}
-        </div>
-      </div>
+            </div>
 
 
-  
-    </>
-  );
+
+        </>
+    );
 }
