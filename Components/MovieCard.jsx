a62169feb6f617b834b8d9dcc386b6f6c4a276e7
FernandoSalicandro <studio.salicandro@gmail.com>
2025-06-18 12:31:50 +0200

    feat: implementato pulsante per più dettagli della card e overlay dei dettagli

--- Components/MovieCard.jsx
+++ Components/MovieCard.jsx
@@ -1,10 +1,11 @@
-// src/components/MovieCard.jsx
+import { useState } from "react";
 
 export default function MovieCard({ movie, getFlagIcon, voteRendering }) {
+  const [showMore, setShowMore] = useState(false);
   const { id, poster_path, title, original_title, original_language } = movie;
 
   return (
-    <div key={id} className="card movie-card">
+    <div className="card movie-card" style={{ position: "relative" }}>
       <img
         className="img-fluid"
         src={`https://image.tmdb.org/t/p/w342${poster_path}`}
@@ -15,7 +16,24 @@ export default function MovieCard({ movie, getFlagIcon, voteRendering }) {
         <p><strong>Titolo:</strong> {title}</p>
         <p><strong>Lingua:</strong> {getFlagIcon(original_language)}</p>
         <p><strong>Voto:</strong> {voteRendering(movie)}</p>
+        <button
+          className="btn btn-outline-danger"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Chiudi Dettagli" : "Più Dettagli"}
+        </button>
       </div>
+
+      {showMore && (
+        <div className="movie-more-details-overlay" onClick={() => setShowMore(false)}>
+          <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+            temporibus odit voluptatibus, impedit quaerat, similique
+            necessitatibus architecto eos sed fuga, quibusdam at magni nostrum
+            vitae minima ullam nesciunt laudantium aliquam.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- Components/SeriesCard.jsx
+++ Components/SeriesCard.jsx
@@ -1,22 +1,39 @@
-export default function SeriesCard({series, getFlagIcon, voteRendering}){
+import { useState } from "react";
 
-    const {id, poster_path, title,original_name, name,original_language} = series;
+export default function SeriesCard({ series, getFlagIcon, voteRendering }) {
+  const [showMore, setShowMore] = useState(false);
+  const { id, poster_path, title, original_name, name, original_language } = series;
 
-    return (
-        <>
-            <div key={id} className="card movie-card">
-                                <img
-                                    className="img-fluid"
-                                    src={`https://image.tmdb.org/t/p/w342${poster_path}`}
-                                    alt={title}
-                                />
-                                <div className="movie-details">
-                                    <p><strong>Titolo Originale:</strong> {original_name}</p>
-                                    <p><strong>Titolo:</strong> {name}</p>
-                                    <p><strong>Lingua:</strong> {getFlagIcon(original_language)} </p>
-                                    <p><strong>Voto:</strong> {voteRendering(series)}</p>
-                                </div>
-                            </div>
-        </>
-    )
+  return (
+    <div className="card movie-card" style={{ position: "relative" }}>
+      <img
+        className="img-fluid"
+        src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+        alt={title}
+      />
+      <div className="movie-details">
+        <p><strong>Titolo Originale:</strong> {original_name}</p>
+        <p><strong>Titolo:</strong> {name}</p>
+        <p><strong>Lingua:</strong> {getFlagIcon(original_language)}</p>
+        <p><strong>Voto:</strong> {voteRendering(series)}</p>
+        <button
+          className="btn btn-outline-danger"
+          onClick={() => setShowMore(!showMore)}
+        >
+          {showMore ? "Chiudi Dettagli" : "Più Dettagli"}
+        </button>
+      </div>
+
+      {showMore && (
+        <div className="movie-more-details-overlay" onClick={() => setShowMore(false)}>
+          <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+            temporibus odit voluptatibus, impedit quaerat, similique
+            necessitatibus architecto eos sed fuga, quibusdam at magni nostrum
+            vitae minima ullam nesciunt laudantium aliquam.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
